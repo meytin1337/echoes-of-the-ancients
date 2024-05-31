bcbf22dbf0f8a730487e277ee3f49b1fe94e2c2e
Mats Heemeyer <meytin@protonmail.com>
2024-05-31 20:26:49 +0200

    feat: switch to side panel for inventory

--- src/ui/inventory.rs
+++ src/ui/inventory.rs
@@ -20,18 +20,25 @@ impl Default for InventoryState {
 }
 
 pub fn show_inventory(
-    mut inventory_state: ResMut<InventoryState>,
+    inventory_state: ResMut<InventoryState>,
     mut egui_contexts: EguiContexts,
     item_query: Query<(Entity, &Item), (With<InventoryItem>, Without<EquippedItem>)>,
     equipped_item_query: Query<(Entity, &Item), With<EquippedItem>>,
     mut commands: Commands,
 ) {
     if inventory_state.is_window_open {
-        egui::Window::new("Inventory")
-            .open(&mut inventory_state.is_window_open)
+        egui::SidePanel::left("Inventory")
             .show(egui_contexts.ctx_mut(), |ui| {
+                ui.label("Equipped Items:");
+                for (entity, item) in equipped_item_query.iter() {
+                    ui.label(&item.name);
+                    if ui.button("Unequip").clicked() {
+                        commands.entity(entity).remove::<EquippedItem>();
+                    }
+                }
                 for (entity, item) in item_query.iter() {
-                    ui.label(item.name.clone());
+                    ui.label("Inventory:");
+                    ui.label(&item.name);
                     if ui.button("Drop").clicked() {
                         commands.entity(entity).remove::<InventoryItem>();
                     }
@@ -39,18 +46,10 @@ pub fn show_inventory(
                         commands.entity(entity).insert(EquippedItem);
                         for (equipped_entity, equipped_item) in equipped_item_query.iter() {
                             // if another item of the same type is equipped, remove it
-                            println!("equipped item: {}", item.name);
                             if entity != equipped_entity && item.item_type == equipped_item.item_type {
-                                println!("unequipped item: {}", equipped_item.name);
                                 commands.entity(equipped_entity).remove::<EquippedItem>();
                             }
                         }
-                    }
-                }
-                for (entity, item) in equipped_item_query.iter() {
-                    ui.label(String::from("equipped item:") + &item.name);
-                    if ui.button("Unequip").clicked() {
-                        commands.entity(entity).remove::<EquippedItem>();
                     }
                 }
             });
